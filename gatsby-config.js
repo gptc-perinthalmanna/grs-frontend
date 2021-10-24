874acd7df2ad94fee26a4dbb5467399356289367
Amjed Ali K <amjedmgm@gmail.com>
2021-10-24 19:53:16 +0530

    :ambulance: Replaces Dart Sass with Node Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("node-sass"),
+      },
+    },
 
     {
       resolve: `gatsby-plugin-manifest`,
